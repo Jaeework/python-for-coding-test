aae88a3658e171ed30ecc207a7927857325b3896
Yujeong (Jay) So <jaeework@gmail.com>
2023-09-12 22:06:32 +0900

    Create java_11.java

---
+++ Ch.05DFS_BFS/java_11.java
@@ -0,0 +1,87 @@
+/*
+ * 미로 탈출
+ */
+
+import java.util.*;
+
+class Node {
+
+    private int x;
+    private int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}
+
+public class java_11 {
+    
+    public static int n;
+    public static int m;
+    public static int[][] graph;
+
+    public static int bfs(int x, int y) {
+        Queue<Node> q = new LinkedList<Node>();
+
+        q.offer(new Node(x, y));
+
+        // 상, 하, 좌, 우
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        while(!q.isEmpty()) {
+            Node node = q.poll();
+            x = node.getX();
+            y = node.getY();
+
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    // 범위 벗어날 경우 무시
+                    continue;
+                }
+
+                if(graph[nx][ny] == 0) {
+                    // 괴물 만날 경우 무시
+                    continue;
+                }
+
+                if(graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    q.offer(new Node(nx, ny));
+                }
+            }
+        }
+
+        return graph[n - 1][m - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.nextLine();
+
+        graph = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            for(int j = 0; j < m; j++) {
+                graph[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs(0, 0));
+    }
+}
